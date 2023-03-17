ad4b8b22186985d80fed2582410342853ea0f0ea
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-03-18 03:35:17 +0530

    Element add funcitonality

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingLstInDB = ref(database, 'shoppingList')
 
-
 const inputFieldEl = document.getElementById('input-field')
 const addButtonEl = document.getElementById('add-button')
 const shoppingListEL = document.getElementById('shopping-list')
@@ -17,6 +16,19 @@ const shoppingListEL = document.getElementById('shopping-list')
 addButtonEl.addEventListener('click', function () {
     let inputValue = inputFieldEl.value
     push(shoppingLstInDB, inputValue)
-    shoppingListEl.innerHTML += `<li>${inputValue}</li>`
+    clearInputFieldEl()
+    appendItemToShoppingListEl(inputValue)
     console.log(inputValue)
 })
+
+// function to clear input value 
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ''
+}
+
+//function to add an element to the list
+
+function appendItemToShoppingListEl(itemValue) {
+    shoppingListEL.innerHTML += `<li>${itemValue}</li>`
+}
